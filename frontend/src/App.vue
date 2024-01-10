fa3da49797b0138044e1dff5f6358a7e8ca3db1a
bradleySchaeferComputerEng <97250667+bradleySchaeferComputerEng@users.noreply.github.com>
2024-01-10 15:08:09 -0500

    Getting a online frontend started

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="boarder">
+    Instagram Cropping Tool
+  </div>
+  <!-- Button to upload a file -->
+  <div class="boarder">
+    <input type="file" id="file" accept="image/*" />
+  </div>
+  <!-- Div to display the file that is an image -->
+  <div class="boarder">
+    <img id="image" />
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const file = ref(null);
+    const image = ref(null);
+    const onFileChange = (e) => {
+      file.value = e.target.files[0];
+    };
+    onMounted(() => {
+      document.getElementById("file").addEventListener("change", onFileChange);
+    });
+    return {
+      file,
+      image,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.boarder {
+  border: 1px solid rgb(255, 255, 255);
+  width: 100%;
+  height: 100%;
+}
+</style>
