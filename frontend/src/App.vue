7161e5fedf98edc645f54ee84714af7d3e78c5b7
bradleySchaeferComputerEng <97250667+bradleySchaeferComputerEng@users.noreply.github.com>
2024-02-23 14:45:18 -0500

    Cropping fun almost done!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,24 +7,29 @@
     <input type="file" id="file" accept="image/*" />
   </div>
   <!-- Div to display the file that is an image -->
-	<cropper
-		:src="img"
-		@change="onChange"
-		:debounce="false"
-		:stencil-props="{
-			aspectRatio: 9 / 16
-		}"
-	/>
-	<preview
-		:width="90"
-		:height="160"
-		:image="result.image"
-		:coordinates="result.coordinates"
-	/>
-  <button 
-    @click="crop">
-		Crop
-	</button>
+  <div class="image-container">
+    <cropper :src="img" ref="cropper" @change="onChange" :debounce="false" :stencil-props="{
+      aspectRatio: this.getLocalAspectRatio()
+    }" />
+  </div>
+  <!-- Slider between 1-5 -->
+  <div class="slider">
+    <input type="range" min="2" max="6" value="1" v-model="sliderValue" id="myRange">
+    {{ sliderValue }} {{ this.getLocalAspectRatio() }}
+  </div>
+  <div class="display-container">
+    <div v-for="index in parseInt(this.sliderValue)" :key="index" class="display-preview">
+      <preview class="preview-image" :width="90" :height="160" :image="result.image" :coordinates="{
+        width: result.coordinates.width / parseInt(this.sliderValue),
+        height: result.coordinates.height,
+        top: result.coordinates.top,
+        left: result.coordinates.left + ((result.coordinates.width / parseInt(this.sliderValue)) * (index - 1))
+      }" />
+    </div>
+  </div>
+  <button @click="crop">
+    Crop
+  </button>
 </template>
 
 <script>
@@ -42,11 +47,17 @@ export default {
   data() {
     return {
       file: null,
+      sliderValue: 2,
       img: 'https://images.unsplash.com/photo-1682687219640-b3f11f4b7234?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       result: {
-				coordinates: null,
-				image: null
-			}
+        coordinates: {
+          width: 0,
+          height: 0,
+          top: 0,
+          left: 0
+        },
+        image: null
+      }
     }
   },
   watch: {
@@ -65,20 +76,25 @@ export default {
     //     reader.readAsDataURL(this.file);
     //   }
     // },
+    getLocalAspectRatio() {
+      return (this.sliderValue * 9) / 16;
+    },
     onChange({ coordinates, image }) {
-			this.result = {
-				coordinates,
-				image
-			};
-		},
-    // crop() {
-		// 	const { coordinates, canvas, } = this.$refs.cropper.getResult();
-		// 	this.coordinates = coordinates;
-		// 	// You able to do different manipulations at a canvas
-		// 	// but there we just get a cropped image, that can be used 
-		// 	// as src for <img/> to preview result
-		// 	this.image = canvas.toDataURL();
-		// },
+      this.result = {
+        coordinates,
+        image
+      };
+    },
+    crop() {
+      //Do logic here to save n amount of photos based on the selected amount to get proper cropping
+      const { coordinates, canvas, } = this.$refs.cropper.getResult();
+      this.image = canvas.toDataURL();
+      const newWidth = coordinates.width / parseInt(this.sliderValue);
+      const link = document.createElement('a');
+      link.download = 'image.png';
+      link.href = this.image;
+      link.click();
+    },
   }
 }
 </script>
@@ -90,30 +106,25 @@ export default {
 }
 
 .image-container {
-  position: relative;
   display: inline-block;
-  width: 95%;
-  height: 95%;
+  max-height: 40%;
+  max-width: 70%;
 }
 
-#image {
-  width: 100%;
+.display-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.grid-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  display: grid;
+.display-preview {
+  max-height: 40%;
+  max-width: 70%;
+  padding: 10px;
 }
 
-.grid-item {
-  background-color: transparent;
-  border: 1px dashed;
-  font-size: 30px;
-  text-align: center;
-  color: rgb(255, 0, 0);
+.preview-image {
+  background-color: aliceblue;
+
 }
 </style>
